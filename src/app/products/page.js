832fc85ada70d560ea13192d9b4fecd0bfd8e298
Sasfi12 <sufyan10.muhammad1070@hotmail.com>
2025-03-24 07:40:51 +0100

    managed to map a api data in nextJS using axios

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,9 +1,45 @@
+"use client";
+import { useEffect, useState } from "react"
+import axios from "axios"
 export default function Products() {
+    const [data , setData] = useState(null);
+    const [error , setError] = useState(null)
+    const [loading , setLoading] = useState(true)
+    useEffect(()=> {
+        axios.get("https://api.jikan.moe/v4/anime?q=新世紀&sfw")
+        .then((response) => {setData(response.data.data) ; setLoading(false) } )
+        .catch((error) => setError(error))
+    } , [])
+    console.log(data)
+    if(loading) {
+        return <h1 className="loading">Loading...</h1>
+    }
+    if(error) {
+        return <h1>{error}</h1>
+    }
+    if(data) {
     return (
     <div>
         <h1>
-            Welcome
+            Browse our products 
         </h1>
+        <div className="anime-cards">
+        { data ? 
+            
+               data.map((e , key) => {
+                return (
+                    <div key={key}>
+                   <h2>{e.title_japanese}</h2>
+                   <img src={e.images.webp.image_url}/>
+                   </div>
+                    )
+               })
+            
+            : 
+            <h1 className="loading">Loading...</h1>
+        }
+        </div>
     </div>
     )
+    }
 } 
